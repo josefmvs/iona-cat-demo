cb9df07317836808a2b888d67bfcfe5c80dc32aa
josef solon <josefsolon@gmail.com>
2022-11-17 08:15:18 +0800

    setup router, eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { CatDetail } from './components/CatDetail';
+import { CatsBrowserPage } from './components/cats/CatsBrowserPage';
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CatsBrowserPage />,
+  },
+  {
+    path: "/cat/:id",
+    element: <CatDetail />,
+  },
+]);
+
+export const App: React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
-
-export default App;
