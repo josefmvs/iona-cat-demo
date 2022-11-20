64c87150f60069d814b817ebebcc937971e1a039
josef solon <josefsolon@gmail.com>
2022-11-20 14:22:31 +0800

    working load more

--- src/components/cats/CatsBrowserPage.tsx
+++ src/components/cats/CatsBrowserPage.tsx
@@ -3,9 +3,11 @@ import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { CatContext } from '../../providers/catsprovider';
 import { ICatBrowserState, ICatContextType, IBreedType } from "../../@types/cat";
 import { CatList } from "./CatList";
+import { useSearchParams } from "react-router-dom";
 
 export const CatsBrowserPage: React.FunctionComponent = () => {
   const { breeds, setBreeds, selectBreedCats, breedCats, loadMoreBreedCats } = useContext(CatContext) as ICatContextType;
+  const [searchParams] = useSearchParams();
   const [loading, setLoading] = useState<boolean>(false);
   const [count, setCount] = useState<number>(0);
   const [showLoadMore, setShowLoadMore] = useState<boolean>(true);
@@ -18,13 +20,16 @@ export const CatsBrowserPage: React.FunctionComponent = () => {
 
   useEffect(() => {
     setBreeds();
+    if (searchParams.get('breed') !== null) {
+      handleBackButton(searchParams.get('breed') ?? '');
+    }
   }, []);
 
   useEffect(() => {
     if (breedCats.length > 0) {
       setCount(breedCats.length);
 
-      if (breedCats.length === count) {
+      if (breedCats.length === count) { // || searchParams.get('breed') === undefined) { 
         setShowLoadMore(false);
       }
     }
@@ -32,7 +37,15 @@ export const CatsBrowserPage: React.FunctionComponent = () => {
   }, [breedCats]);
 
   const handleSelectCatBreed = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const selectedBreed = breeds.find(a => a.id === event.currentTarget.value) as IBreedType;
+    selectCatBreed(event.currentTarget.value);
+  }
+
+  const handleBackButton = (id: string): void => {
+    selectCatBreed(id);
+  };
+
+  const selectCatBreed = (id: string): void => {
+    const selectedBreed = breeds.find(a => a.id === id) as IBreedType;
     const params = { breed: selectedBreed, page: 1, limit: catBrowserState.limit, loadMore: catBrowserState.loadMore }
     setLoading(true);
     setShowLoadMore(true);
@@ -55,7 +68,7 @@ export const CatsBrowserPage: React.FunctionComponent = () => {
           <Col lg="3">
             <Form>
               <Form.Label>Breed</Form.Label>
-              <Form.Control as="select" custom onChange={handleSelectCatBreed}>
+              <Form.Control as="select" value={catBrowserState.breed?.id} custom onChange={handleSelectCatBreed}>
                 <option value="-1">Select Breed</option>
                 {breeds.map(breed => {
                   return (
--- src/providers/reducers/breedsReducer.ts
+++ src/providers/reducers/breedsReducer.ts
@@ -1,4 +1,4 @@
-import { IBreedsCollection } from "../../@types/cat";
+import { IBreedsCollection, ICatType } from "../../@types/cat";
 
 export enum BreedsActionKind {
     GET_BREEDS = 'GET_BREEDS',
@@ -18,11 +18,29 @@ const breedsReducer = (state: IBreedsCollection, action: ICatsAction): IBreedsCo
             return { ...state, breeds: payload };
         case BreedsActionKind.SELECT_BREED:
             return { ...state, breedCats: payload.breedCats, selectedBreed: payload.breed };
-        case BreedsActionKind.LOAD_MORE:
-            return { ...state, breedCats: payload.breedCats };
+        case BreedsActionKind.LOAD_MORE: 
+            return setUniqueBreedCats(state, payload);
         default:
             return state;
     }
 };
+
+const setUniqueBreedCats = (state: IBreedsCollection, payload: any): IBreedsCollection => {
+    const newState = [...state.breedCats, ...payload.breedCats] as ICatType[]
+    const uniqueIds: string[] = [];
+    const unique = newState.filter(element => {
+        const isDuplicate = uniqueIds.includes(element.id);
+      
+        if (!isDuplicate) {
+            uniqueIds.push(element.id);
+      
+          return true;
+        }
+      
+        return false;
+      });
+
+    return { ...state, breedCats: unique }
+}
 
 export default breedsReducer;
