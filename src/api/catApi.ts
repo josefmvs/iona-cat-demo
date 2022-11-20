444cbcbe814dd6f5f14b2c3bd966282d93087f26
josef solon <josefsolon@gmail.com>
2022-11-20 15:41:15 +0800

    clean up

--- src/api/catApi.ts
+++ src/api/catApi.ts
@@ -7,7 +7,24 @@ const searchImagesUrl = "https://api.thecatapi.com/v1/images/search";
 
 export async function getCatBreeds (): Promise<IBreedType[]> {
     const _url = breedsUrl;
-    try {
+
+    const { data } = await axios.get(
+        _url,
+        {
+            headers: {
+            Accept: 'application/json',
+            },
+        },
+    );
+
+    return data;
+}
+
+export async function getCatsByBreed (payload: ICatBrowserState): Promise<IBreedType[]> {
+    const _params = payload;
+    const _url = searchImagesUrl + "?page=" + _params.page.toString() + "&limit=" + _params.limit.toString() + "&breed_id=" + (_params.breed !== undefined ? _params.breed?.id.toString() : '');
+    
+    if (_params.breed !== undefined && _params.breed?.id !== '') {
         const { data } = await axios.get(
             _url,
             {
@@ -18,37 +35,7 @@ export async function getCatBreeds (): Promise<IBreedType[]> {
         );
 
         return data;
-    } catch (error) {
-        handleError(error);
     }
 
     return [];
-}
-
-export async function getCatsByBreed (payload: ICatBrowserState): Promise<IBreedType[]> {
-    const _params = payload;
-    const _url = searchImagesUrl + "?page=" + _params.page.toString() + "&limit=" + _params.limit.toString() + "&breed_id=" + (_params.breed !== undefined ? _params.breed?.id.toString() : '');
-    
-    if (_params.breed !== undefined && _params.breed?.id !== '') {
-        try {
-            const { data } = await axios.get(
-                _url,
-                {
-                    headers: {
-                    Accept: 'application/json',
-                    },
-                },
-            );
-    
-            return data;
-        } catch (error) {
-            handleError(error);
-        }
-    }
-
-    return [];
-}
-
-function handleError (error: any): any {
-    throw error;
 }
